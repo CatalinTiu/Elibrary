d5d62ac8b54c2c2c8a0779e70132f04328d526b4
Catalin <catamihairo@gmail.com>
2020-10-28 18:32:58 +0200

    tradus in romana

--- src/app/qr-scanner/qr-scanner.page.ts
+++ src/app/qr-scanner/qr-scanner.page.ts
@@ -14,6 +14,33 @@ export class QrScannerPage implements OnInit {
   constructor(private http: HttpClient,private barcodeScanner: BarcodeScanner,private service: MainServiceService,private router: Router) { }
 
 
+  // scan_mobile(){
+  //   this.barcodeScanner.scan( {
+  //       preferFrontCamera : true, // iOS and Android
+  //       showFlipCameraButton : true, // iOS and Android
+  //       showTorchButton : true, // iOS and Android
+  //       torchOn: false, // Android, launch with the torch switched on (if available)
+  //       prompt : "", // Android
+  //       resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+  //       formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+  //       orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+  //       disableAnimations : true, // iOS
+  //       disableSuccessBeep: false // iOS and Android
+  //   }).then(barcodeData => {
+  //       const album_id = JSON.parse(barcodeData.text)["album_id"];
+  //       if(album_id != null && typeof(album_id)=== 'number'){
+  //         this.service.setScan(false);
+  //         this.service.changeAlbum(album_id);
+  //         this.router.navigate(['/album-detail']);
+  //       }else{
+  //         this.service.setScan(false);
+  //         this.router.navigate(['/musiclist']);
+  //       }
+        
+  //     }).catch(err => {
+  //       console.log('Error', err);
+  //     });  
+  // }
   scan_mobile(){
     this.barcodeScanner.scan( {
         preferFrontCamera : true, // iOS and Android
@@ -27,16 +54,19 @@ export class QrScannerPage implements OnInit {
         disableAnimations : true, // iOS
         disableSuccessBeep: false // iOS and Android
     }).then(barcodeData => {
-        const album_id = JSON.parse(barcodeData.text)["album_id"];
-        if(album_id != null && typeof(album_id)=== 'number'){
+        if(barcodeData.cancelled){
           this.service.setScan(false);
-          this.service.changeAlbum(album_id);
-          this.router.navigate(['/album-detail']);
         }else{
-          this.service.setScan(false);
-          this.router.navigate(['/musiclist']);
+          const album_id = JSON.parse(barcodeData.text)["album_id"];
+          if(album_id != null && typeof(album_id)=== 'number'){
+            this.service.setScan(false);
+            this.service.changeAlbum(album_id);
+            this.router.navigate(['/album-detail']);
+          }else{
+            this.service.setScan(false);
+            this.router.navigate(['/musiclist']);
+          }
         }
-        
       }).catch(err => {
         console.log('Error', err);
       });  
@@ -56,10 +86,10 @@ export class QrScannerPage implements OnInit {
   scan(val:any){
     if(val == true){
       this.service.setScan(false);
-      if(document.URL.startsWith('http://localhost')) {
-        this.scan_mobile();
-      } else {
+      if(document.URL.startsWith('https://elibraria.ro')) {
         this.browser_scan();
+      } else {
+        this.scan_mobile();
       }
     }
   }
